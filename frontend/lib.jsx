0d96583f00cc5621c21d185521bfb15d2335b640
sdauwidhwa <zhangjungeac@gmail.com>
2025-10-19 21:50:12 -0700

    Added auth to frontend.

--- frontend/lib.jsx
+++ frontend/lib.jsx
@@ -2,8 +2,7 @@ import { useEffect, useState, useRef, useImperativeHandle, forwardRef } from 're
 
 // Component for single selection with multi-filter and scrolling
 export const CompSingleSelectionScrolledFiltered = ({ style, content_style, onSelect, options }) => {
-
-  const { candidates, filter_options, initial_filter_options } = options;
+  const { candidates = [], filter_options = [], initial_filter_options = [] } = options;
   const [selected_item, set_selected_item] = useState(null);
   const [selected_filters, set_selected_filters] = useState(initial_filter_options);
 
@@ -16,40 +15,43 @@ export const CompSingleSelectionScrolledFiltered = ({ style, content_style, onSe
     ? candidates.filter(candidate => selected_filters.includes(candidate.type))
     : candidates;
 
-
-
-
-
   return (
-    <div style={style}>
+    <div className='expand' style={style}>
       <div>
         Selected {selected_item}
       </div>
-      <div>
+      {filter_options.length > 0 && <div>
         Filters:
         {filter_options.map(e => {
           const onClick = () => { set_selected_filters((prev) => prev.includes(e) ? prev.filter((i) => i !== e) : [...prev, e]); }
           const selected = selected_filters.includes(e)
           return <button key={e} style={{ backgroundColor: selected ? "#acb7ceff" : null }} onClick={onClick}>{e}</button>;
         })}
-      </div>
+      </div>}
 
-      <div style={content_style}>
-        {filtered_candidates.map(e2 => {
-          const e = e2.id;
-          const onClick = () => { set_selected_item(e); onSelect(e); }
-          const selected = selected_item === e;
-          return <button key={e} style={{ backgroundColor: selected ? "#acb7ceff" : null }} onClick={onClick}>{e}</button>;
+      <div className='scrollWindow' style={{background:"#DEE"}}><div className='scrollContent' >
+        {filtered_candidates.map(cand => {
+          const selected = selected_item === cand.id;
+          const onClick = onSelect && (() => { set_selected_item(cand.id); onSelect(cand.id); });
+          return <button key={cand.id} style={{ backgroundColor: selected ? "#acb7ceff" : null }} onClick={onClick}>{cand.id}</button>;
         })}
-      </div>
-
-
-    </div >
+      </div></div>
+    </div>
   );
 };
 
+CompSingleSelectionScrolledFiltered.defaultProps = {
+  options: {
+    candidates: [],
+    filter_options: [],
+    initial_filter_options: []
+  }
+};
 
 
+export const FullHorizontalButton = ({ onclick, inner_style, outer_style }) => {
+
+}
 
 
 export const DynamicList = forwardRef((props, self_ref) => {
@@ -93,7 +95,7 @@ export const DynamicList = forwardRef((props, self_ref) => {
 
 
 export const Notification = ({ dlparent, dlkey, closable, close_timeout, on_close, message }) => {
-  
+
   const close_self = () => {
     dlparent.current.remove_comp(dlkey);
     if (on_close) { on_close(); }
